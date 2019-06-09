5969de80660cd87ca072b70ae96d02e0724a1ef5
mjkang <rkdalswn0310@naver.com>
2019-06-10 01:23:39 +0900

    [part2] java 설정 전체

---
+++ jex01/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,31 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+	
+	//p.161 servlet3.0 이상 사용 필수 
+//	@Override
+//	protected void customizeRegistration(ServletRegistration.Dynamicregistration) {
+//		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true"); 
+//	}
+
+}
